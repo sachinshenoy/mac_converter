b3d57961085719dc5edcc0578b980eb107718db9
Sachin Shenoy <sachinshenoy76@gmail.com>
2017-02-17 15:23:41 -0500

    First Code Commit with modified code to support any text input with MAC Addresses

--- converter.py
+++ converter.py
@@ -1,64 +1,74 @@
-import sys
-sys.path.append(r"c:\users\YOURUSERNAME\appdata\local\programs\python\python35-32\lib\site-packages")
+# import sys
+# sys.path.append(r"c:\users\YOURUSERNAME\appdata\local\programs\python\python35-32\lib\site-packages")
 import pyperclip
+import re
+
 
 def get_mac():
-    '''Used to get the mac from the clip borad'''
+    """Used to get the mac from the clip board"""
     return pyperclip.paste()
 
+
 def convert(mac_address):
-    '''Converts the MAC from xxxx.xxxx.xxxx format to xx:xx:xx:xx:xx format or vise versa'''
-    if str(mac_address).count(".",0,len(mac_address))==2 and len(mac_address)==14:
-        #Convert MAC from xxxx.xxxx.xxxx format to xx:xx:xx:xx:xx:xx format
-        mac_address=str(mac_address)
-        mac_address=mac_address.replace(".","")
-        count = 1
-        mac_address=list(mac_address)
-        while(count<14):
-            mac_address.insert(count+1,":")
-            count+=3
-        return (''.join(mac_address)).lower()
+    """Converts the MAC from xxxx.xxxx.xxxx format to xx:xx:xx:xx:xx format or vise versa"""
 
-    elif str(mac_address).count(":",0,len(mac_address))==5 and len(mac_address)==17:
-        #Convert MAC from xx:xx:xx:xx:xx:xx format to xxxx.xxxx.xxxx format
-        mac_address=str(mac_address)
-        mac_address=mac_address.replace(":","")
-        count = 3
-        mac_address=list(mac_address)
-        while(count<13):
-            mac_address.insert(count+1,".")
-            count+=5
-        return (''.join(mac_address)).lower()
+    # Regex to match MAC Address patterns xx.xx.xx.xx.xx.xx or xxxx.xxxx.xxxx
+    # with separator '.' or '-' or ':'
+    regex = r"(([A-F0-9]{2}[:.-]){5}[A-F0-9]{2}|([A-F0-9]{4}[:.-]){2}[A-F0-9]{4})"
 
-    elif str(mac_address).count("-",0,len(mac_address))==5 and len(mac_address)==17:
-        #Convert MAC from xx-xx-xx-xx-xx-xx format to xxxx.xxxx.xxxx format
-        mac_address=str(mac_address)
-        mac_address=mac_address.replace("-","")
-        count = 3
-        mac_address=list(mac_address)
-        while(count<13):
-            mac_address.insert(count+1,".")
-            count+=5
-        return (''.join(mac_address)).lower()
+    # Findall used to look for all instances of the match, with multiline and ignore case flags
+    mac_tuple = re.findall(regex, mac_address, re.MULTILINE | re.IGNORECASE)
 
-    elif str(mac_address).count("\r\n")>0:
-        #Converts multiline clipboards normally from copying several rows in excel for example.
-        multi_line=str(mac_address).split("\r\n")
-        size=len(multi_line)
-        result=list()
-        for x in range(0,size-1):
-            single_result=convert(multi_line[x])
-            result.append(single_result+"\r\n")
-        return ''.join(result)
-
-    else:
-        return "Invalid MAC format"
+    # Initializing temp variables. i for iteration, result for return result collection and
+    # temp for temporary string variable storage and manipulation.
+    # separator to store the separator used with the input mac address format.
+    i = 0
+    result = []
+    temp = ''
+    separator = ''
+    while i < len(mac_tuple):
+        # Check if the MAC Address format is a xx.xx.xx.xx.xx.xx then results are in xxxx.xxxx.xxxx format
+        if (mac_tuple[i][0].count(':') == 5 or mac_tuple[i][0].count('.') == 5 or mac_tuple[i][0].count('-') == 5):
+            separator = mac_tuple[i][0][2]
+            temp = mac_tuple[i][0].replace(separator, '')
+            temp = temp[0:4] + separator + temp[4:8] + separator + temp[8:] + '\r\n'
+            result.append(temp)
+            i += 1
+        # When the MAC Address is in the xxxx.xxxx.xxxx format, result are xx.xx.xx.xx.xx.xx
+        else:
+            separator = mac_tuple[i][0][4]
+            temp = mac_tuple[i][0].replace(separator, '')
+            temp = temp[0:2] + separator + temp[2:4] + separator + temp[4:6] + separator + temp[6:8] \
+                   + separator + temp[8:10] + separator + temp[10:] + '\r\n'
+            result.append(temp)
+            i += 1
+    return ''.join(result)
 
 
 def main():
+
+    """
+    MAC ADDRESS Format Converter for Windows Clipboard:
+    --------------------------------------------------
+    This program takes input text which is copied to the windows clipboard containing MAC Addresses and
+    outputs the mac address in the transformed format.
+
+    For example:
+    input format is xxxx.xxxx.xxxx the output will be xx.xx.xx.xx.xx.xx
+    input format xx.xx.xx.xx.xx.xx will result in output in xxxx.xxxx.xxxx format
+
+    The input separator used in the output will match the one from the input. Also the text can contain multiple
+    mac addresses intermingled with other text for Data
+
+    Usage:
+        Just copy the input text to windows clipboard and run the program then
+        paste the output from the windows clipboard to the required document.
+
+    Works/Tested for Windows 7, Windows 8.x and Window 10"""
+
     pyperclip.copy(convert(get_mac()))
+#    print(convert(mac_address))
 
 
 if __name__ == '__main__':
     main()
-
